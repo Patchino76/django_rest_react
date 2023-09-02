f73cb3eca0eeb620dd4f1d5c06448e0d1bf6f92b
Svetoslav Lyubenov <patchino76@gmail.com>
2023-09-02 17:37:26 +0300

    test get, post

--- api/views.py
+++ api/views.py
@@ -3,31 +3,30 @@ from .models import Article
 from .serializers import ArticleSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
-
+# from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
  
-# Create your views here.
-# def index(request):
-#   return HttpResponse('It is working')
 
-
-@csrf_exempt
+# @csrf_exempt
+@api_view(['GET', 'POST'])
 def article_list(request):
   #get all articles
   if request.method == 'GET':
     articles = Article.objects.all()
     serializer = ArticleSerializer(articles, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
   
   elif request.method == 'POST':
-    data = JSONParser().parse(request)
-    serializer = ArticleSerializer(data=data)
+    
+    serializer = ArticleSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data, status=201)
-    return JsonResponse(serializer.errors, status=400)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
-@csrf_exempt
+# @csrf_exempt
 def article_details(request, pk):
   try:
     article = Article.objects.get(pk=pk)
